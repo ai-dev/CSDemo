bfcb1ad115355ffa8bb024427800c804fb3e7413
Angelo <iasevoli90@gmail.com>
2019-01-10 18:35:55 +0100

    Add working basic APIs

--- test/apiController.test.ts
+++ test/apiController.test.ts
@@ -28,7 +28,7 @@ describe('API controller', () => {
 
         expect(response.statusCode).to.be.equal(200);
         expect(response.result).to.be.an('object');
-        expect(response.result).to.be.equal({text, done: false});
+        expect(response.result).to.be.deep.equal({text, done: false});
     });
 
     it('should return the list of Todos after one has been created', async () => {
@@ -40,18 +40,37 @@ describe('API controller', () => {
         expect(response.statusCode).to.be.equal(200);
         expect(response.result).to.be.an('array');
         expect(response.result.length).to.be.equal(1);
-        expect(response.result).to.be.equal({text, done: false});
+        expect(response.result).to.be.deep.equal([{text, done: false}]);
     });
 
     it('should change the status of a Todo item and return it', async () => {
+        const done = true;
         const response = await Server.inject({
             method: 'PUT',
             url: '/api/todo/0',
+            payload: {
+                done,
+            },
         });
 
         expect(response.statusCode).to.be.equal(200);
         expect(response.result).to.be.an('object');
-        expect(response.result.length).to.be.equal(1);
-        expect(response.result).to.be.equal({text, done: true});
+        expect(response.result).to.be.deep.equal({text, done});
+    });
+
+    it('should throw an error if the item that we want to update doens\'t exist', async () => {
+        const index = 30;
+        const done = true;
+        const response = await Server.inject({
+            method: 'PUT',
+            url: `/api/todo/${index}`,
+            payload: {
+                done,
+            },
+        });
+
+        expect(response.statusCode).to.be.equal(500);
+        expect(response.result).to.be.an('string');
+        expect(response.result).to.be.equal(`Item with index ${index} not found`);
     });
 });
